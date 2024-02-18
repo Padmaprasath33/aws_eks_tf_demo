2d935cbe6f8812b1274b474636fa22171466a2d9
Padmaprasath <padmaprasath33@gmail.com>
2024-02-18 15:35:39 +0530

    added lock as false

--- eks_namespace.tf
+++ eks_namespace.tf
@@ -2,7 +2,7 @@ data "aws_eks_cluster_auth" "cluster" {
   name = aws_eks_cluster.eks_cluster.id
 }
 
-provider "kubernetes" {
+/*provider "kubernetes" {
   host                   = aws_eks_cluster.eks_cluster.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
@@ -13,3 +13,4 @@ resource "kubernetes_namespace_v1" "prasath-ns" {
     name = "prasath-ns"
   }
 }
+*/
