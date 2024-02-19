95cb247aefa4cefc30f8e347f0d971232020fc23
Padmaprasath <padmaprasath33@gmail.com>
2024-02-19 07:50:08 +0530

    added lock as false

--- iam-oidc-connect-provider.tf
+++ iam-oidc-connect-provider.tf
@@ -5,10 +5,6 @@ resource "aws_iam_openid_connect_provider" "oidc_provider" {
   thumbprint_list = [var.eks_oidc_root_ca_thumbprint]
   url             = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
 
-  tags = merge(
-    {
-      Name = "${var.cluster_name}-eks-irsa"
-    },
-    local.common_tags
-  )
+  tags = local.common_tags
+  
 }
